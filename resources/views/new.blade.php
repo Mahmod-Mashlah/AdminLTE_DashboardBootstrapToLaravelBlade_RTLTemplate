7196c8d2bf01d21cbc7fcf075a5a0f9eda80e026
Mahmod Mashlah <mashlahmahmod@gmail.com>
2024-04-07 09:07:04 +0300

    new for any new blade view page content template & Dashboard example & yield root+son1+son2+content

---
+++ resources/views/new.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+
+@section('title')
+@endsection {{-- or @stop --}}
+
+@section('css')
+    {{-- Css here --}}
+@endsection
+
+@section('root')
+    {{-- root --}}
+@endsection
+
+@section('son1')
+    {{-- son1 --}}
+@endsection
+
+@section('son2')
+    {{-- son2 --}}
+@endsection
+
+@section('content')
+    {{-- content --}}
+@endsection
+
+@section('scipts')
+    {{-- Scripts here --}}
+@endsection
