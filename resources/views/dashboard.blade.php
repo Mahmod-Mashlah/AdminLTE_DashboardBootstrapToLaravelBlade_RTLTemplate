c3bc57648c9c718378115126da92a9b3193c9141
Mahmod Mashlah <mashlahmahmod@gmail.com>
2024-04-07 08:21:26 +0300

    dashboard instead welcome & make css+scripts+title yield (variable) in layouts then (call them with) section in other blade pages

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+@section('title')
+    لوحة التحكم
+@endsection {{-- or @stop --}}
+@section('css')
+    {{-- Css here --}}
+@endsection
+
+
+@section('scipts')
+    {{-- Scripts here --}}
+@endsection
--- resources/views/layouts/footer-scripts.blade.php
+++ resources/views/layouts/footer-scripts.blade.php
@@ -1,4 +1,3 @@
-
 <!-- jQuery -->
 {{--   this line below will converted  --}}
 {{-- <script src="plugins/jquery/jquery.min.js"></script> --}}
@@ -9,7 +8,7 @@
 <script type="text/javascript" src="{{ URL::asset('assets/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
-  $.widget.bridge('uibutton', $.ui.button)
+    $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 rtl -->
 <script src="https://cdn.rtlcss.com/bootstrap/v4.2.1/js/bootstrap.min.js"></script>
@@ -37,13 +36,15 @@
 <script type="text/javascript" src="{{ URL::asset('assets/plugins/daterangepicker/daterangepicker.js') }}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
 {{-- <script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script> --}}
-<script type="text/javascript" src="{{ URL::asset('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+<script type="text/javascript"
+    src="{{ URL::asset('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
 <!-- Summernote -->
 {{-- <script src="plugins/summernote/summernote-bs4.min.js"></script> --}}
 <script type="text/javascript" src="{{ URL::asset('assets/plugins/summernote/summernote-bs4.min.js') }}"></script>
 <!-- overlayScrollbars -->
 {{-- <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script> --}}
-<script type="text/javascript" src="{{ URL::asset('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+<script type="text/javascript"
+    src="{{ URL::asset('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
 {{-- <script src="dist/js/adminlte.js"></script> --}}
 <script type="text/javascript" src="{{ URL::asset('assets/js/adminlte.js') }}"></script>
@@ -56,12 +57,16 @@
 
 {{-- JavaScript Bootstrap CDN --}}
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
 
 {{-- Preloader  --}}
 <script>
-        // After 0.5 seconds, add the 'loaded' class to the body
-        setTimeout(function() {
-            document.body.classList.add('loaded');
-        }, 500);
-    </script>
+    // After 0.5 seconds, add the 'loaded' class to the body
+    setTimeout(function() {
+        document.body.classList.add('loaded');
+    }, 500);
+</script>
+
+@yield('scripts')
